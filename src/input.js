2814b252397f2c82bd789e791f01f2a8780623a6
prajwal <spiderbasnet140@gmail.com>
2025-05-06 17:27:44 +0545

    d

--- src/input.js
+++ src/input.js
@@ -3,7 +3,6 @@ let sumbitTodoBtn = document.querySelector("#sumbitTodoBtn");
 let dialog = document.querySelector("dialog");
 let closeBtn = document.querySelector("dialog button")
 
-let a = 0;
 
 let currentEditingPTag = null;
 
@@ -32,41 +31,54 @@ closeBtn.addEventListener("click", () => {
     dialog.close();
 });
 
+function loadTodos() {
+    let todo = JSON.parse(localStorage.getItem("todolist")) || [];
+    todo.forEach(item => {
+        addTodoToDOM(item);
+    });
+}
 
 function createTodoTags() {
     let inputTagValue = document.querySelector("#inputTag").value;
     //div tag for p
-    if(inputTagValue){
-       
+    if (inputTagValue) {
+        // let todo = JSON.parse(localStorage.getItem("todolist")) || [];
+
         let divContainer = document.createElement("div");
         divContainer.classList = "divContainer";
         let pTag = document.createElement("p");
-    
 
-    pTag.textContent = inputTagValue;
-    pTag.classList = "todoInfo";
-    
-    let deleteDiv = deleteTodoTags(divContainer);
-    
-    let editButton = document.createElement("button");
-    editButton.textContent = "edit";
-    editButton.classList = "editButton";
-    
-    editButton.addEventListener("click", (e) => {
-        e.preventDefault();
 
-        currentEditingPTag = pTag;
-        editInputTag.value = pTag.textContent;
-        dialog.showModal();
-    })
-    
-    divContainer.appendChild(pTag)
-    divContainer.appendChild(deleteDiv)
-    divContainer.appendChild(editButton)
-    
-    todoData.appendChild(divContainer);
+        pTag.textContent = localStorage.getItem("todolist");
+        pTag.classList = "todoInfo";
+
+        
+        
+        let deleteDiv = deleteTodoTags(divContainer);
+
+        let editButton = document.createElement("button");
+        editButton.textContent = "edit";
+        editButton.classList = "editButton";
+
+        editButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            currentEditingPTag = pTag;
+            editInputTag.value = pTag.textContent;
+            dialog.showModal();
+        })
+        // localStorage.clear();
+
+        todo.push(inputTagValue);
+        localStorage.setItem("todolist", JSON.stringify(todo))
+        console.log(localStorage.getItem("todolist"));
+        
+        divContainer.appendChild(pTag)
+        divContainer.appendChild(deleteDiv)
+        divContainer.appendChild(editButton)
+        todoData.appendChild(divContainer);
+    }
 }
-}
+console.log(localStorage.getItem("todoList"))
 
 
 sumbitTodoBtn.addEventListener("click", (e) => {
