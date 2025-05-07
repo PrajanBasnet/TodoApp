b765e8a1eb0e589d4ec5f59d96742d220df7719f
prajwal <spiderbasnet140@gmail.com>
2025-05-07 21:15:38 +0545

    edit logic add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import './assets/style.css';
 import "./assets/domstyle.css";
 import { createTodoTags } from './input.js';
-
-createTodoTags();
--- src/input.js
+++ src/input.js
@@ -2,8 +2,67 @@ let todoData = document.querySelector(".todoData");
 let sumbitTodoBtn = document.querySelector("#sumbitTodoBtn");
 let dialog = document.querySelector("dialog");
 let closeBtn = document.querySelector("dialog button")
+let submitEditValue = document.querySelector("#submitEditValue");
 
 
 
+let todoArryValue = localStorage.getItem("ArrayItem") ;
+let storingInLocal = todoArryValue ? JSON.parse(todoArryValue) : [];
 
-export { createTodoTags }
+let i = 0;
+function generateTags(data){
+     i = i + 1;
+    let pTag = document.createElement("p");
+    pTag.textContent = data;
+
+    let button = document.createElement("button");
+    button.textContent = "Edit";
+
+    button.setAttribute("id", i)
+    button.addEventListener("click",(e)=>{
+        dialog.showModal()
+        editTheTodo(data);
+    })
+    todoData.append(pTag,button);
+}
+
+
+function editTheTodo(item){
+    const editedItem = storingInLocal.filter((data) => data === item);
+    submitEditValue.addEventListener("click",(e)=>{
+        dialog.close();
+        let editInputTag = document.querySelector("#editInputTag").value;
+        for (let i = 0; i < storingInLocal.length; i++) {
+            if( storingInLocal[i] === item){
+                storingInLocal[i] = editInputTag;
+                console.log(storingInLocal[i])
+                localStorage.setItem("ArrayItem",JSON.stringify(storingInLocal))
+                location.reload()
+            }
+            
+        }
+    })
+}
+
+function saveItemInStorage(data){
+    storingInLocal.push(data)
+    localStorage.setItem("ArrayItem",JSON.stringify(storingInLocal));
+
+}
+
+sumbitTodoBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let inputValue = document.querySelector("#inputTag").value;
+    saveItemInStorage(inputValue)
+    generateTags(inputValue);
+
+})
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+    storingInLocal.forEach(todoItems => {
+        generateTags(todoItems)
+    });
+})
+
+
+
